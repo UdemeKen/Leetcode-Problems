7bdf69aef71db410db2bd9cf3116cd3d78f27173
UdemeKen <udemekendrick@gmail.com>
2025-01-21 08:05:13 +0100

    recently updated

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,5 +1,6 @@
 package org.udemekendrick;
 
+import org.udemekendrick.containsDuplicate.ContainsDuplicate;
 import org.udemekendrick.duplicateNumbers.DuplicateNumbers;
 import org.udemekendrick.maxProfit.MaxProfit;
 import org.udemekendrick.rotateArray.RotateArray;
@@ -8,9 +9,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        RotateArray rotateArray = new RotateArray();
-        System.out.println(Arrays.toString(rotateArray.rotate(nums, k)));
+        int[] nums = {1, 7, 3, 5, 6, 7};
+        ContainsDuplicate cd = new ContainsDuplicate();
+        boolean result = cd.containsDuplicate(nums);
+        System.out.println(result);
     }
 }
---
+++ src/main/java/org/udemekendrick/containsDuplicate/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package org.udemekendrick.containsDuplicate;
+
+import java.util.HashSet;
+
+/**
+ * Author: Udeme Kendrick
+ *
+ * @version 1.0
+ * @license MIT License
+ * @see <a href="mailto:udemekendrick@gmail.com">udemekendrick@gmail.com</a>
+ * @see <a href="https://udemekendrick.vercel.app">https://udemekendrick.vercel.app</a>
+ * @since 1/21/2025
+ */
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
